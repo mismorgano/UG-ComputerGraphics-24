75259d49c442af7528d5da07c7213bb5056f1950
mismorgano <mismorgano@gmail.com>
2024-09-17 09:18:20 -0600

    Adding ParseShader In order to read from disk (a file) the vertex and fragment shader.
    The vertex and fragment shader could be in different files, but we merge them in one.
    
    Refactor code to use VertexIndexes to draw a square.
    
    TODO:
    - Make a class to read and compile shaders and modified them.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,39 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <bits/fs_fwd.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+struct ShaderProgramSource {
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string &filepath) {
+    std::ifstream stream(filepath);
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (std::getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            } else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        } else {
+            ss[static_cast<int>(type)] << line << '\n';
+        }
+    }
+
+    return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string &source) {
     unsigned int id = glCreateShader(type);
@@ -61,33 +93,34 @@ int main() {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float vertices[] = {
+    float position[] = {
         -0.5f, -0.5f,
-        0.0f, 0.5f,
-        0.5f, -0.5f
+        0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
+
+    unsigned int indices[] {
+        0, 1, 2,
+        2, 3, 0
     };
     unsigned int buffer; // It's the ID
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), position, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *) 0);
 
-    std::string vertexShader = R"(
-        #version 330 core
-        layout (location = 0) in vec4 position;
-        void main() {
-           gl_Position = position;
-        })";
-    std::string fragmentShader = R"(
-        #version 330 core
-        layout (location = 0) out vec4 color;
-        void main() {
-           color = vec4(0.5, 0.2, 0.8, 1.0);
-        })";
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
-    glUseProgram(shader);
+    unsigned int index_buffer = 0;
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+
+    auto source = ParseShader("res/shaders/basic.shader");
+    unsigned int program = CreateShader(source.VertexSource, source.FragmentSource);
+    glUseProgram(program);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
@@ -95,7 +128,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6 , GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -103,7 +136,7 @@ int main() {
         /* Poll for and process events */
         glfwPollEvents();
     }
-    glDeleteProgram(shader);
+    glDeleteProgram(program);
     glfwTerminate();
     return 0;
 }
